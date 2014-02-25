b6ef509932b966851c61e6062cb41118b51289ed
Jayendra Kartheek <root@Jayendra.kartheek>
2014-02-25 18:47:47 +0530

    adding more code

--- getData.php
+++ getData.php
@@ -17,3 +17,49 @@
 		/*Getting user Data*/
 		case 'userinfo'		: fnGetUserData();
 								break;
+								
+								
+								
+		function fnCurl($method, $url, $params = "", $contentType = "JSON") {
+			switch(strtoupper($contentType)) {
+				case "TEXT"		:	$arrCon = Array("Content-Type: text/plain");
+									break;
+				case "JSON"		:	$arrCon = Array("Content-Type: application/json");
+									break;
+				default			:	$arrCon = "";
+			}
+			
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $url.((strtoupper($method) == "GET") ? $params : ""));
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $arrCon);
+			curl_setopt($ch, CURLOPT_HEADER, FALSE);
+			curl_setopt($ch, CURLOPT_HTTPGET,1);
+			if(strtoupper($method) == "POST") {
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+				curl_setopt($ch, CURLOPT_POST, 1);
+			}
+			curl_setopt($ch,CURLOPT_TIMEOUT,30);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			return $result;
+		}
+		
+		
+		function fnGetTagsData(){
+			
+		
+		
+		
+		}
+		
+		function fnGetTagsData(){
+			
+		
+		
+		
+		
+		}						
