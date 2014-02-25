8b3b5adb2074003634b06c169fa85d9500a6418b
Jayendra Kartheek <kartheekgj@gmail.com>
2014-02-25 23:00:00 +0530

    function to get data done litle tweaking may be required

--- getData.php
+++ getData.php
@@ -1,6 +1,4 @@
 <?php 
-
-
 	error_reporting(E_ERROR);
 	ini_set('display_errors',1);
 	ini_set('max_execution_time',300);
@@ -8,19 +6,17 @@
 	require_once("function.inc.bms");
 
 	$cmd = (!empty($_POST['cmd']) ? $_POST['cmd'] : '');
+	$url = 'http://api.stackexchange.com';
 
 	switch(strtoupper($cmd)){
 		
 		/*Getting Tags Data*/
 		case 'tagsdata'		: fnGetTagsData();
 								break;
-		/*Getting user Data*/
-		case 'userinfo'		: fnGetUserData();
-								break;
-								
-								
-								
-		function fnCurl($method, $url, $params = "", $contentType = "JSON") {
+		
+
+		// Fucntions								
+		function fnCurl($url, $params = "", $contentType = "JSON") {
 			switch(strtoupper($contentType)) {
 				case "TEXT"		:	$arrCon = Array("Content-Type: text/plain");
 									break;
@@ -30,14 +26,11 @@
 			}
 			
 			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $url.((strtoupper($method) == "GET") ? $params : ""));
+			curl_setopt($ch, CURLOPT_URL, $url);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $arrCon);
 			curl_setopt($ch, CURLOPT_HEADER, FALSE);
-			curl_setopt($ch, CURLOPT_HTTPGET,1);
-			if(strtoupper($method) == "POST") {
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-				curl_setopt($ch, CURLOPT_POST, 1);
-			}
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+			curl_setopt($ch, CURLOPT_POST, 1);
 			curl_setopt($ch,CURLOPT_TIMEOUT,30);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
@@ -50,16 +43,17 @@
 		
 		
 		function fnGetTagsData(){
+			global $url;
+			$sort	=	(!empty($_POST['sort']) 	? $_POST['sort']	: 'votes');
+			$tags	=	(!empty($_POST['tags'] 		? $_POST['tags']	: '');
+			$order	=	(!empty($_POST['order']) 	? $_POST['order']	: 'desc');
 			
-		
-		
-		
+			$arrTags = explode(';',$tags);
+			foreach($arrTags as $tagsData){
+				$params  = '/2.2/questions?order='. $order .'&sort='. $sort .'&tagged='. $tagsData .'&site=stackoverflow';
+				$arrFinalTagsData[$tagsData] = fnCurl($url, $params); 
+			}
+			
+			echo json_encode($arrFinalTagsData);		
 		}
 		
-		function fnGetTagsData(){
-			
-		
-		
-		
-		
-		}						
