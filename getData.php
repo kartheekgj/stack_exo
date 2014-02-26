ee3c6f3d485d1c5bd95155e907c32368b299822a
Jayendra Kartheek <root@Jayendra.kartheek>
2014-02-26 18:38:02 +0530

    made it working css not good should make it beter

--- getData.php
+++ getData.php
@@ -6,7 +6,7 @@
 
 	$cmd = (!empty($_POST['cmd']) ? $_POST['cmd'] : '');
 	$url = 'http://api.stackexchange.com';
-
+	$dataFiles = __DIR__.'/files/';
 	switch(strtolower($cmd)){
 		
 		/*Getting Tags Data*/
@@ -16,50 +16,74 @@
 	}							
 		
 
-		// Fucntions								
-		function fnCurl($url, $params = "", $contentType = "JSON") {
-			switch(strtoupper($contentType)) {
-				case "TEXT"		:	$contentType = Array("Content-Type: text/plain");
-									break;
-				case "JSON"		:	$contentType = Array("Content-Type: application/json");
-									break;
-				default			:	$contentType = "";
+	// Fucntions								
+	function fnGetContents($url, $params = "") {
+		
+		$url .= $params;
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+
+		$response = curl_exec($ch);
+		$info = curl_getinfo($ch);
+		curl_close ($ch);
+
+		$result =  gzdecode($response);
+		
+		return $result;
+	}
+	
+	
+	function fnGetTagsData(){
+		
+		global $url;
+		global $dataFiles;
+		$sort		=	(!empty($_POST['sort']) 		? $_POST['sort']	: 'votes');
+		$tags		=	(!empty($_POST['tags']) 		? $_POST['tags']	: '');
+		$order		=	(!empty($_POST['order']) 		? $_POST['order']	: 'desc');
+		$rescnt		=	(!empty($_POST['rescnt']) 		? $_POST['rescnt']	: 100);
+		$refresh	=	(!empty($_POST['refresh']) 		? $_POST['refresh']	: "false");
+		
+		$arrTags = explode(';',$tags);
+		// getting the data
+		foreach($arrTags as $finTags){
+			if(!empty(trim($finTags))){
+				$params  = '/2.2/questions?pagesize='. $rescnt .'&order='. $order .'&sort='. $sort .'&tagged='. $finTags .'&site=stackoverflow';	
+				
+				if($refresh == "true"){
+					$jsonData = fnGetContents($url, $params);
+					@file_put_contents($dataFiles.$finTags.'.json',$jsonData);
+				}else{
+					if(!file_exists($dataFiles.$finTags.'.json')){
+						$jsonData = fnGetContents($url, $params);
+						@file_put_contents($dataFiles.$finTags.'.json',$jsonData);
+					} else {
+						$jsonData = file_get_contents($dataFiles.$finTags.'.json');
+					}
+				}
+				$arrFinalTagsData[$finTags] = json_decode($jsonData,true);	
+			}
+		}
+		// getting the counts array
+		foreach($arrTags as $finTags){
+			if(!empty(trim($finTags))){
+				foreach($arrFinalTagsData[$finTags]['items'] as $data){
+					$arrBounty[$finTags][]	= $data['bounty_amount'];
+					$arrAnsCnt[$finTags][]	= $data['answer_count'];
+					$arrScore[$finTags][]	= $data['score'];
+				}	
 			}
 			
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $contentType);
-			curl_setopt($ch, CURLOPT_HEADER, FALSE);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-			curl_setopt($ch, CURLOPT_POST, 1);
-			curl_setopt($ch,CURLOPT_TIMEOUT,30);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			print_R($result);
-
-			curl_close($ch);
-
-			return $result;
 		}
 		
-		
-		function fnGetTagsData(){
-			
-			global $url;
-			$sort	=	(!empty($_POST['sort']) 		? $_POST['sort']	: 'votes');
-			$tags	=	(!empty($_POST['tags']) 		? $_POST['tags']	: '');
-			$order	=	(!empty($_POST['order']) 		? $_POST['order']	: 'desc');
-			
-			$arrTags = explode(';',$tags);
-			print_R($arrTags);
-			foreach($arrTags as $tagsData){
-				if(!empty($tagsData))
-				echo $params  = '/2.2/questions?order='. $order .'&sort='. $sort .'&tagged='. $tagsData .'&site=stackoverflow';
-				$arrFinalTagsData[$tagsData] = fnCurl($url, $params); 
+		// summing up the counts to single array
+		foreach($arrTags as $finTags){
+			if(!empty(trim($finTags))){
+				$arrFinData[$finTags] = array('bounty_amnt' => array_sum($arrBounty[$finTags])/100, 'answer_count' => array_sum($arrAnsCnt[$finTags])/100,'score' => array_sum($arrScore[$finTags])/100);
 			}
-			
-			//echo json_encode($arrFinalTagsData);
-					
-		}	
+		}
+		echo json_encode($arrFinData);
+	}	
--- index.php
+++ index.php
@@ -1,35 +1,52 @@
 <html>
 	<head>
 		<title>StackOverFlow in Graphs</title>
+		<link href="./css/stack_css.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
-		<button onClick='fnGetData();'>Get Data</button>
+		<ul id="ul_chkBox"> 
+			<li>
+				<label>Tags available for statistics</label>
+			</li>	
+			<li>
+				<label>PHP</label>	
+				<input type="checkbox" value="php" checked=checked  id="chk_php">
+			</li>	
+			<li>
+				<label>JQUERY</label>	
+				<input type="checkbox" value="jquery" id="chk_jquery">
+			</li>	
+			<li>
+				<label>MYSQL</label>	
+				<input type="checkbox" value="mysql" id="chk_jquery">
+			</li>
+			<li style=" font-size:small;">Note : Check the text boxes to get the data in the graph</li>	
+			
+		</ul>
+		
+		<div>
+			<ul class="ul_buttons">
+				<li>
+					<label>Getting the stored Data from stackoverflow</label>
+					<button id="but_old" onClick='fnGetData({rescnt: 100});'>Get Data</button>
+					<button id="but_old_ldng" >loading Data</button>
+				</li>
+				
+				<li>
+					<label>Getting the new Data from stackoverflow</label>
+					<button id="but_new" onClick='fnGetData({refresh : true,rescnt: 100});'>Get New Data Counts</button>
+					<button id="but_new_ldng">loading Data</button>
+				</li>
+			</ul>
+			
+		</div>
+		<div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+		
+		
 	</body>
 </html>
 
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script type="text/javascript">
-	var jq = jQuery.noConflict();
-	
-	function fnGetData(){
-		var sort = '';
-		var tags = 'php;';
-		var order = '';
-		//var url = 'https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&tagged=php&site=stackoverflow'; // working
-		var url = "./getData.php"
-		var data = 'cmd=tagsdata&sort='+ sort +'&tags='+ tags +'&order='+ order;
-		jq.ajax({
-			url: url,
-			type: "POST",
-			data: data,
-			dataType: "JSON",
-			sucess: function(data){
-						console.log(data);
-					},
-			error: function(jqXHR, textStatus){
-						console.log( "Request failed: " + textStatus );
-					}
-		});	
-	}
-
-</script>
+<script type="text/javascript" src="./js/stack_func_compress.js"></script>
+<!-- Graph Starts -->
+<script type="text/javascript" src="./js/graph_compress.js"></script>
